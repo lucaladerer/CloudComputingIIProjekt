ae790f8ab04be4b35f896a291c2696b0f40d1760
lucaladerer <luca.laderer2002@gmail.com>
2025-03-17 22:59:03 +0100

    modified public key file

--- TerraformAnsible/terraform/main.tf
+++ TerraformAnsible/terraform/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~./.ssh/keyForCC")
+    public_key = file("~/.ssh/keyForCC.pub")
   }
 
   os_disk {
