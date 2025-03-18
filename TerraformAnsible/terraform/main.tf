e243c250b128f082ad50e55ae707869b10725483
lucaladerer <luca.laderer2002@gmail.com>
2025-03-18 11:08:26 +0100

    added port 5000 to nwg

--- TerraformAnsible/terraform/main.tf
+++ TerraformAnsible/terraform/main.tf
@@ -68,6 +68,28 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+## security rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_flask" {
+  name                        = "allow_flask"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5000"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+
 # Create Linux virtual machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "jokes-vm"
