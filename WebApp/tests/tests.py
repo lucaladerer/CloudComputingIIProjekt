b8392475de559b86f26b3f775d0a77a9a0b06055
Ubuntu <azureuser@simpleLinuxVM.nx4acokjokmunal2mnkxbjaioa.ax.internal.cloudapp.net>
2025-04-14 08:55:04 +0000

    improved .gitignore and tests.py

--- WebApp/tests/tests.py
+++ WebApp/tests/tests.py
@@ -6,24 +6,19 @@ import conftest
 def webApp_IP(request):
     return request.config.getoption("--ip")
 
-"""
-test, where WebApp does not need to run
-"""
 # test for the API
 def test_check_API():
+    """Test for the API"""
     response = requests.get(f"https://official-joke-api.appspot.com/random_joke")
     assert response.status_code == 200
 
-
-"""
-tests, where WebApp needs to run
-"""
-# test for landing page
+# tests, where WebApp needs to run
 def test_check_WebApp(webApp_IP):
+    """Test for the landing page"""
     response = requests.get(f"http://{webApp_IP}:5000/")
     assert response.status_code == 200
 
-# test for ranking page
 def test_check_WebApp_ranking(webApp_IP):
+    """Test for the ranking page"""
     response = requests.get(f"http://{webApp_IP}:5000/ranking")
     assert response.status_code == 200
